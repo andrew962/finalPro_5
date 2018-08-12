e02e1d2c6bd81e3feb97344fc92e3ac094bc67f2
andrew962 <abadia962@gmail.com>
2018-08-11 19:29:15 -0500

    Documentacion

--- flask-chatterbot/app.py
+++ flask-chatterbot/app.py
@@ -11,8 +11,16 @@ try:
                                   #'chatterbot.logic.MathematicalEvaluation',
                                   'chatterbot.logic.LowConfidenceAdapter',
                                   'chatterbot.logic.BestMatch',
+                                  #Desde este punto se estaran aplicando las nuevas logicas creadas
                                   'metro_logic.MyLogicAdapter'
+                                  """Esta logica es la que hace la coneccion a la api de metrobus
+                                  y tambien valida las diferentes palabras que se introducen
+                                  para separarlas del numero de la tarjeta."""
                               ])
+    """
+    Esta dos lineas comentadas son las que entrenan al chatbot,
+    se comentan ya que no siempre se tiene que entrenar el chatbot.
+    """
     # bot.set_trainer(ChatterBotCorpusTrainer)
     # bot.train("chatterbot.corpus.spanish")
 except Exception as e:
@@ -24,6 +32,12 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
+    """
+    try: es por si ocurre algun error.
+    :except Nos muestra el error de una forma mas detallada
+    :var userText: variable que trae lo que le introduscamos.
+    :return: retorna la respuesta del bot.
+    """
     try:
         userText = request.args.get('msg')
     except Exception as e:
--- flask-chatterbot/metro_logic.py
+++ flask-chatterbot/metro_logic.py
@@ -10,12 +10,24 @@ class MyLogicAdapter(LogicAdapter):
         """
         Return true if the input statement contains.
         """
-
+        """
+        :var patron: es como el validador que buscara dentro de la oracion los numeros.
+        :var self.num: donde se guardan los numeros despues de hacer la busqueda con los parametros
+        que le pasamos por patron.
+        """
         patron = re.compile(r'\d\d\d\d\d\d\d\d')
         self.num = patron.search(statement.text)
 
-
+        """
+        :list words: son las palabras claves que el chatbot utilizara para detectar
+        que tiene que hacer una busqueda de saldo de metro bus.
+        """
         words = ['mi','saldo']
+        """
+        if all(x in statement.text.split() for x in words):
+        hace la busqueda palabra por palabra validando cada una
+        :return True: si encuentra alguna de las dos palabras.
+        """
         if all(x in statement.text.split() for x in words):
             return True
         else:
@@ -26,11 +38,20 @@ class MyLogicAdapter(LogicAdapter):
         import requests,json
 
         #api metro bus
-
+        """
+        :var url: api de metro bus
+        :self.num.group(): aqui hago referecia al numero obtenido de la busqueda que se hizo mas arriba
+        por medio de self.num = patron.search(statement.text).
+        :var response: se hace el llamado de la api para obtener los valores.
+        """
         url ='http://panamenio.herokuapp.com/api/com/metrobus/'+self.num.group()
         response = requests.get(url)
         response.text
         # Let's base the confidence value on if the request was successful
+        """
+        if response.status_code == 200: valida que la coneccion con la api fue exitosa, si cumple la condicion
+        la confianza para a 1 para que el bot lo agrege a la base de datos y pueda se impreso.
+        """
         if response.status_code == 200:
             confidence = 1
         else:
@@ -38,6 +59,10 @@ class MyLogicAdapter(LogicAdapter):
 
         data = json.loads(response.text)
         saldo = str(data['balance'])
+        """
+        :response_statement = Statement('Tu saldo es {}'.format(saldo)): de esta manera es que el bot nos lo estuviera
+        mostrando en pantalla.
+        """
         response_statement = Statement('Tu saldo es {}'.format(saldo))
         response_statement.confidence=confidence
 
